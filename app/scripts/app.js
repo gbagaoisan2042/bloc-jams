35dea51b21d2e920c0a506580ded25b08dfc8e28
Gilbert B <gbertb@gmail.com>
2014-08-05 05:19:34 +0000

    Built a SongPlayer service to share song data across views.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -79,11 +79,11 @@
 
  }]);
 
-  blocJams.controller('Album.controller', ['$scope', function($scope) {
+  blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
    $scope.album = angular.copy(albumPicasso);
 
    var hoveredSong = null;
-   var playingSong = null;
+
 
    $scope.onHoverSong = function(song) {
      hoveredSong = song;
@@ -94,7 +94,7 @@
   };
 
   $scope.getSongState = function(song) {
-     if (song === playingSong) {
+     if (song === SongPlayer.currentSong && SongPlayer.playing) {
        return 'playing';
      }
      else if (song === hoveredSong) {
@@ -105,11 +105,36 @@
 
 
   $scope.playSong = function(song) {
-    playingSong = song;
+     SongPlayer.setSong($scope.album, song);
+     SongPlayer.play();
   };
 
   $scope.pauseSong = function(song) {
-    playingSong = null;
+     SongPlayer.pause();
   };
 
  }]);
+
+
+blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
+  $scope.songPlayer = SongPlayer;
+}]);
+
+ blocJams.service('SongPlayer', function() {
+   return {
+     currentSong: null,
+     currentAlbum: null,
+     playing: false,
+
+     play: function() {
+       this.playing = true;
+     },
+     pause: function() {
+       this.playing = false;
+     },
+     setSong: function(album, song) {
+       this.currentAlbum = album;
+       this.currentSong = song;
+     }
+   };
+ });
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -359,11 +359,11 @@ var changeAlbumView = function(album) {
 
  }]);
 
-  blocJams.controller('Album.controller', ['$scope', function($scope) {
+  blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
    $scope.album = angular.copy(albumPicasso);
 
    var hoveredSong = null;
-   var playingSong = null;
+
 
    $scope.onHoverSong = function(song) {
      hoveredSong = song;
@@ -374,7 +374,7 @@ var changeAlbumView = function(album) {
   };
 
   $scope.getSongState = function(song) {
-     if (song === playingSong) {
+     if (song === SongPlayer.currentSong && SongPlayer.playing) {
        return 'playing';
      }
      else if (song === hoveredSong) {
@@ -385,14 +385,39 @@ var changeAlbumView = function(album) {
 
 
   $scope.playSong = function(song) {
-    playingSong = song;
+     SongPlayer.setSong($scope.album, song);
+     SongPlayer.play();
   };
 
   $scope.pauseSong = function(song) {
-    playingSong = null;
+     SongPlayer.pause();
   };
 
  }]);
+
+
+blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
+  $scope.songPlayer = SongPlayer;
+}]);
+
+ blocJams.service('SongPlayer', function() {
+   return {
+     currentSong: null,
+     currentAlbum: null,
+     playing: false,
+
+     play: function() {
+       this.playing = true;
+     },
+     pause: function() {
+       this.playing = false;
+     },
+     setSong: function(album, song) {
+       this.currentAlbum = album;
+       this.currentSong = song;
+     }
+   };
+ });
 
 });
 
